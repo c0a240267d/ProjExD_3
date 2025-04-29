f9573f28a5a5eeb57cab3ec796b317dc1232b9c2
shunki <c0a240267d@edu.teu.ac.jp>
2025-04-29 14:33:53 +0900

    こうかとん喜ぶ

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -165,11 +165,13 @@ def main():
                 pg.display.update()
                 time.sleep(1)
                 return
+            
         if beam is not None:
             if bomb is not None:
                 if beam.rct.colliderect(bomb.rct):  # ビームとボムの衝突判定
                     beam = None  # ビームを消す
                     bomb = None  # ボムを消す
+                    bird.change_img(6, screen)
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
